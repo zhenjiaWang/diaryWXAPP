d602fa1f1c10c661cf2b0fb76fe1d7af09895704
wangjia <wangjiamr@163.com>
2018-10-04 19:06:58 +0800

    copy

--- biz/clothes.js
+++ biz/clothes.js
@@ -1,5 +1,8 @@
+const { wxPost, isEnableBtn } = require('../utils/common.js')
+
 export default {
   data: {
+    myClothesShow: false,
     clothesShow: false,
     clothesItems: []
   },
@@ -8,5 +11,57 @@ export default {
   },
   closeClothes: function () {
     this.setData({ clothesShow: false, maskShow: false })
+  },
+  showMyClothes: function () {
+    this.setData({ myClothesShow: true, maskShow: true })
+  },
+  closeMyClothes: function () {
+    this.setData({ myClothesShow: false, maskShow: false })
+  },
+  buyClothes: function (e) {
+    const that = this
+    if (that.data.userState.clothesLimit == 1 && that.data.submitFlag) {
+      return false
+    } else {
+      that.setData({ submitFlag: true })
+      let clothesId = e.currentTarget.dataset.id
+      if (clothesId) {
+        wxPost(
+          '/user/buyClothes',
+          {
+            userId: that.data.userId,
+            clothesId: clothesId
+          },
+          ({ data }) => {
+            if (data.errorCode >= 0) {
+              that.setData({ submitFlag: false, clothesShow: false, dialogShow: true, dialogText: data.text })
+            }
+          }
+        )
+      }
+    }
+  },
+  sellClothes: function (e) {
+    const that = this
+    if (that.data.userState.clothesLimit == 1 && that.data.submitFlag) {
+      return false
+    } else {
+      that.setData({ submitFlag: true })
+      let clothesId = e.currentTarget.dataset.id
+      if (clothesId) {
+        wxPost(
+          '/user/sellClothes',
+          {
+            userId: that.data.userId,
+            clothesId: clothesId
+          },
+          ({ data }) => {
+            if (data.errorCode >= 0) {
+              that.setData({ submitFlag: false, myClothesShow: false, clothesShow: false, dialogShow: true, dialogText: data.text })
+            }
+          }
+        )
+      }
+    }
   }
 }
